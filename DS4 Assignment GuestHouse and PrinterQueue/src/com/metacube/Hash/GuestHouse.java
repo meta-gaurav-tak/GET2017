c7f7d12bae49800f4fc9e0c2b595a42d729c111f
meta-gaurav-tak <gaurav.tak@metacube.com>
2017-08-27 01:24:13 +0530

    DS4 Complete

---
+++ DS4 Assignment GuestHouse and PrinterQueue/src/com/metacube/Hash/GuestHouse.java
@@ -0,0 +1,67 @@
+package com.metacube.Hash;
+
+import java.util.Hashtable;
+
+/**
+ * Class to generate a unique room for each guest and allot it to the guest.
+ * 
+ * @author Gaurav Tak
+ */
+public class GuestHouse {
+    private int totalRoomsInGuestHouse;
+    private int currentlyAvailableNumberOfRooms;
+    public int getTotalRoomsInGuestHouse() {
+        return totalRoomsInGuestHouse;
+    }
+
+    public void setTotalRoomsInGuestHouse(int totalRoomsInGuestHouse) {
+        this.totalRoomsInGuestHouse = totalRoomsInGuestHouse;
+    }
+
+    public GuestHouse(int totalRoomsInGuestHouse) {
+        this.totalRoomsInGuestHouse = totalRoomsInGuestHouse;
+        this.currentlyAvailableNumberOfRooms = totalRoomsInGuestHouse;
+    }
+
+    public Hashtable<Integer, Guest> getHashtable() {
+        return hashtable;
+    }
+
+    public void setHashtable(Hashtable<Integer, Guest> hashtable) {
+        this.hashtable = hashtable;
+    }
+
+    // Number used to generate hash function.
+    public static final int PRIME = 17;
+    private int incrementer = 1;
+    // Hash table consisting details of guests and room alloted to them.
+    public Hashtable<Integer, Guest> hashtable = new Hashtable<Integer, Guest>();
+    
+    /**
+     * Generates the unique room number for each guest.
+     * 
+     * @param guest    Guest class object
+     * @throws GuestHouseFullException 
+     */
+    public int hashedRoomNo(Guest guest) throws GuestHouseFullException {
+        if(currentlyAvailableNumberOfRooms <= 0) {
+            System.out.println("No rooms Available in Guest House : Thanks for visting");
+            throw new GuestHouseFullException();
+        }
+        int age = guest.getAge();
+        int roomNo;
+        // Generate unique room numbers using hash function.
+        roomNo = ((age * + incrementer++) * PRIME) % this.totalRoomsInGuestHouse;
+        // Check if the room number is already filled allot another room to the guest.
+        while (hashtable.containsKey(roomNo+1)) {
+            roomNo = (++roomNo) % this.totalRoomsInGuestHouse;
+        }
+        //adding 1 to room no value since a modulus by total room wiil give value from 0 to totalroom-1
+        guest.setAllotedRoom(roomNo+1);
+        hashtable.put(roomNo+1, guest);
+        currentlyAvailableNumberOfRooms--;
+        return roomNo+1;
+    }
+  
+}
+
---
+++ DS4 Assignment GuestHouse and PrinterQueue/src/com/metacube/Hash/GuestHouseFullException.java
@@ -0,0 +1,7 @@
+package com.metacube.Hash;
+
+public class GuestHouseFullException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+    
+}
