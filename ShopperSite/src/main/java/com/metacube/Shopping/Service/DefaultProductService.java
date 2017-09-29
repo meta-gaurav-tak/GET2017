9a23c3cd54376fdfdc3597b223348a47a4a210ee
meta-gaurav-tak <gaurav.tak@metacube.com>
2017-09-29 15:46:42 +0530

    spring bean Configuration

---
+++ ShopperSite/src/main/java/com/metacube/Shopping/Service/DefaultProductService.java
@@ -0,0 +1,49 @@
+package com.metacube.Shopping.Service;
+
+import com.metacube.Shopping.Product.Model.Product;
+import com.metacube.Shopping.Product.dao.ProductDao;
+
+
+
+public class DefaultProductService implements ProductService {
+    ProductDao productDao;
+    
+    public DefaultProductService() {
+        
+    }
+
+    public ProductDao getProductDao() {
+        return productDao;
+    }
+
+    public void setProductDao(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    public DefaultProductService(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    public Iterable<Product> getAllProducts()
+    {
+        return productDao.findAll();
+    }
+
+    public Product getProductById(final int id)
+    {
+        return productDao.findOne(id);
+    }
+
+    public Boolean deleteProduct(int id) {
+        
+        return productDao.delete(id);
+    }
+    public Boolean editProduct(Product entity,int id) {
+        return productDao.edit(entity,id);
+    }
+
+    
+    public Boolean saveProduct(Product newProduct) {
+        return productDao.save(newProduct);
+    }
+}
